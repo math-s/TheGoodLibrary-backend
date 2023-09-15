5ed472c47a4d7799cd2ba02766b662ca186e196b
Matheus Andrade <matheus.andrade1996@gmail.com>
2023-09-14 21:07:02 -0300

    [add dummy lambdas]

---
+++ src/book-worker/app/index.py
@@ -0,0 +1,11 @@
+import json
+
+
+def handler(event, context):
+    print("Received event:", json.dumps(event))
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello, world!"
+        })
+    }
