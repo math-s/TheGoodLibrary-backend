9adaefa3abb6d5da8de8419174f46f5d35bbd18d
Matheus Andrade <matheus.andrade1996@gmail.com>
2023-09-17 13:25:56 -0300

    [build lambdas and push to ECR]

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 env=${1:-local}
+docker_hub_uri='747334718413.dkr.ecr.us-east-1.amazonaws.com'
 
-
-if [ $env == "local" ]; then
-    docker_hub_uri="localhost:5000"
-else
-    aws_account_id=$AWS_ACCOUNT_ID # get account id from env var
-    aws_region=$AWS_REGION # get region from env var
-    docker_hub_uri="$aws_account_id.dkr.ecr.$aws_region.amazonaws.com"
-fi 
-
+function tag_push_docker_image(){
+    docker tag $1 $docker_hub_uri/$2:$3
+    docker push $docker_hub_uri/$2:$3
+}
 
 function create_lambda_docker_image() {
     lambda_name=$1
     (
         cd ../src/$lambda_name
         TAG_ID=$(git rev-parse HEAD | cut -c1-15)
-        docker build --platform linux/amd64 -t $lambda_name:$TAG_ID . --no-cache
-        docker tag $lambda_name:$TAG_ID $lambda_name:latest
-        docker push $docker_hub_uri/$lambda_name:$TAG_ID 
+        IMAGE_ID=$(docker build --platform linux/amd64 -t $lambda_name:$TAG_ID --quiet . --no-cache | cut -d':' -f2)
+        echo $IMAGE_ID
+        tag_push_docker_image $IMAGE_ID $lambda_name $TAG_ID
     )
 }
 
 # List of lambda names
 lambda_names=("book-worker" "book-service" "publishing-service")
+
+# Login to AWS ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $docker_hub_uri
 
 # Call the create_lambda_bucket function for each lambda
 for lambda_name in "${lambda_names[@]}"
